a567c52022bd54fc600e7045e455618e1b80098e
palepanorama <matthew.keith.thomas@gmail.com>
2021-02-22 15:19:09 -0600

    commit tree 7153a278d6a44dbc6e5955843094fa92ec994994

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",
+function() {
+    const string = document.querySelector("#text")
+    string.innerHTML = "This is really cool!"
+}
+)
